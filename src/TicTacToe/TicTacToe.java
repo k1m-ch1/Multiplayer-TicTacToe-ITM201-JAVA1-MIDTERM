71b0946f1a455ccf0dae7f0f83a3c01722b40ad4
k1m-ch1 <jingluy123@gmail.com>
2026-03-11 17:45:34 +0700

    finished primitive version of board formatting

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,34 +1,98 @@
 package TicTacToe;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class TicTacToe {
   private Boolean board[][];
-  private HashMap<Character, Boolean> playerToBool = new HashMap<>();
   private char[] players;
-  final int N = 3;
+  static final int N = 3;
 
   public TicTacToe(char firstPlayer, char secondPlayer) {
     board = new Boolean[N][N];
     players = new char[] { firstPlayer, secondPlayer };
-    playerToBool.put(firstPlayer, true);
-    playerToBool.put(secondPlayer, false);
+  }
+
+  public char[] mapBooleanArrayToPlayer(Boolean[] booleanArray) {
+
+    char[] playerArray = new char[booleanArray.length];
+
+    for (int i = 0; i < booleanArray.length; i++) {
+      if (booleanArray[i] == null) {
+        playerArray[i] = ' ';
+      } else if (booleanArray[i]) {
+        playerArray[i] = players[0];
+      } else {
+        playerArray[i] = players[1];
+      }
+    }
+
+    return playerArray;
+  }
+
+  public char[][] getBoardAsChar() {
+    // this simply turns the 2d array of boolean into a character array
+    // True is first player
+    // False is second player
+    // null is empty character
+    char boardAsChar[][] = new char[N][N];
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        if (board[i][j] == null) {
+          boardAsChar[i][j] = ' ';
+        } else if (board[i][j]) {
+          boardAsChar[i][j] = players[0];
+        } else {
+          boardAsChar[i][j] = players[1];
+        }
+      }
+    }
+    return boardAsChar;
+  }
+
+  // so apparently, you can use generic types
+  //
+
+  public Boolean[] getFlattenedBoard() {
+    Boolean[] flattenedBoard = new Boolean[N * N];
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        flattenedBoard[i * N + j] = board[i][j];
+      }
+    }
+    return flattenedBoard;
   }
 
   public String getFormattedBoard() {
     String formattedBoard;
-    String rowEntry = " %s |".repeat(N - 1) + " %s \n";
+    String boardTemplate;
+    String rowEntry = " %c |".repeat(N - 1) + " %c \n";
     String rowBorder = "---+".repeat(N - 1) + "---\n";
     String rowEntryAndBorder = rowEntry + rowBorder;
-    formattedBoard = rowEntryAndBorder.repeat(N - 1) + rowEntry;
+    boardTemplate = rowEntryAndBorder.repeat(N - 1) + rowEntry;
+
+    char[] flattenedBoard = mapBooleanArrayToPlayer(getFlattenedBoard());
+
+    // need to convert to generic types... so verbose...
+    Character[] flattenedBoardAsCharacter = new Character[flattenedBoard.length];
+    for (int i = 0; i < flattenedBoard.length; i++) {
+      flattenedBoardAsCharacter[i] = (Character) flattenedBoard[i];
+    }
+
+    formattedBoard = String.format(
+        boardTemplate,
+        (Object[]) flattenedBoardAsCharacter);
+
+    // I still dont' quite get why we need to do Object[] to suppress the warning...
+
     return formattedBoard;
   }
 
-  public Boolean[][] getBoard() {
-    return board;
-  }
-
   public static void main(String[] args) {
-    System.out.println("Hello TicTacToe");
+    // ok we'll now implement the TicTacToe logic
+    TicTacToe board = new TicTacToe('x', 'o');
+
+    Boolean[] flattenedBoard = board.getFlattenedBoard();
+
+    System.out.println(board.getFormattedBoard());
   }
 }
